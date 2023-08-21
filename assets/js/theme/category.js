e01f59246881159060cfbda61e16149c1c648969
DominickMuniz <dominick.muniz7@gmail.com>
2023-08-20 19:17:25 -0600

    refactored according to the newly organized helper functions.

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -98,7 +98,7 @@ export default class Category extends CatalogPage {
 
 
 
-        // CREATE a cart helper function
+        // CREATE a cart - helper function
         function createCart(route, cartItems) {
             return fetch(route, {
               method: "POST",
@@ -118,12 +118,49 @@ export default class Category extends CatalogPage {
 
 
 
-          // GET a cart helper function
+          // GET a cart - helper function
 
           function getCart(route) {
             return fetch(route, {
               method: "GET",
               credentials: "same-origin"
+            })
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.error(error));
+          };
+
+
+
+
+// ADD A CART ITEM - helper function
+          function addCartItem(routeStart, cartId, cartItems) {
+            var route = routeStart + cartId + '/items';
+            return fetch(route, {
+              method: "POST",
+              credentials: "same-origin",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(cartItems),
+            })
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.error(error));
+          };
+
+
+
+
+// DELETE A CART ITEM - helper function
+          function deleteCartItem(routeStart, cartId, itemId) {
+            var route = routeStart + cartId + '/items/' + itemId;
+            return fetch(route, {
+              method: "DELETE",
+              credentials: "same-origin",
+              headers: {
+                "Content-Type": "application/json",
+              }
             })
             .then(response => response.json())
             .then(result => console.log(result))
@@ -143,152 +180,56 @@ export default class Category extends CatalogPage {
 
 
 
-    
-        const addAllToCartButton = document.getElementById('addAllToCart');
-    
-        const addCartItem = (cartId, productId) => {
-            const cartItem = {
-                lineItems: [{
-                    quantity: 1,
-                    productId: parseInt(productId, 10)
-                }]
-            };
-    
-            fetch(`/api/storefront/carts/${cartId}/items`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(cartItem)
-            })
-            .then(response => {
-                if (!response.ok) {
-                    console.error(`Failed to add product with ID: ${productId} to cart. Status: ${response.status}`);
-                    return response.text();
-                }
-            })
-            .then(responseText => {
-                if (responseText) {
-                    console.error(`Server response: ${responseText}`);
-                }
-            })
-            .catch(error => {
-                console.error(`Error adding product with ID: ${productId} to cart: ${error}`);
-            });
-        };
-    
-        const handleAddAllToCart = (cartId) => {
-            const productCards = document.querySelectorAll('.card');
-            const cartItems = {
-                lineItems: []
-            };
-    
-            productCards.forEach(card => {
-                const productId = card.getAttribute('data-entity-id');
-                cartItems.lineItems.push({
-                    quantity: 1,
-                    productId: parseInt(productId, 10)
-                });
-            });
-    
-            createCart(`/api/storefront/carts/${cartId}/items`, cartItems);
-    
-            alert('All products have been added to your cart!');
-        };
-    
-   
-    
-        if (addAllToCartButton) {
-            addAllToCartButton.addEventListener('click', () => {
-                fetch('/api/storefront/carts', {
-                    method: 'GET',
-                    credentials: 'same-origin'
-                })
-                .then(response => response.json())
-                .then(carts => {
-                    if (carts.length > 0) {
-                        // Use existing cart
-                        handleAddAllToCart(carts[0].id);
-                    } else {
-                        // Create new cart
-                        const newCartItems = {
-                            lineItems: []
-                        };
-    
-                        const productCards = document.querySelectorAll('.card');
-                        productCards.forEach(card => {
-                            const productId = card.getAttribute('data-entity-id');
-                            newCartItems.lineItems.push({
-                                quantity: 1,
-                                productId: parseInt(productId, 10)
-                            });
-                        });
-    
-                        createCart('/api/storefront/carts', newCartItems)
-                        .then(newCart => {
-                            handleAddAllToCart(newCart.id);
-                        });
-                    }
-                })
-                .catch(error => {
-                    console.error(`Error getting or creating cart: ${error}`);
-                });
-            });
-        }
+
+          
+          const addAllToCartButton = document.getElementById('addAllToCart');
+
+          const handleAddAllToCart = (cartId) => {
+              const productCards = document.querySelectorAll('.card');
+              const cartItems = {
+                  lineItems: Array.from(productCards).map(card => ({
+                      quantity: 1,
+                      productId: parseInt(card.getAttribute('data-entity-id'), 10)
+                  }))
+              };
+              addCartItem('/api/storefront/carts/', cartId, cartItems)
+                  .then(() => alert('All products have been added to your cart!'));
+          };
+      
+          if (addAllToCartButton) {
+              addAllToCartButton.addEventListener('click', () => {
+                  getCart('/api/storefront/carts')
+                      .then(carts => {
+                          if (carts.length > 0) {
+                              handleAddAllToCart(carts[0].id);
+                          } else {
+                              createCart('/api/storefront/carts', { lineItems: [] })
+                                  .then(newCart => handleAddAllToCart(newCart.id));
+                          }
+                      });
+              });
+          }
+      
+          const deleteAllFromCartButton = document.getElementById('deleteAllFromCart');
+      
+          if (deleteAllFromCartButton) {
+              deleteAllFromCartButton.addEventListener('click', function() {
+                  getCart('/api/storefront/carts?include=lineItems.digitalItems.options,lineItems.physicalItems.options')
+                      .then(cartData => {
+                          if (cartData.length > 0) {
+                              const cartId = cartData[0].id;
+                              const physicalItems = cartData[0].lineItems.physicalItems;
+                              const itemIds = physicalItems.map(item => item.id);
+                              itemIds.forEach(itemId => {
+                                  deleteCartItem('/api/storefront/carts/', cartId, itemId);
+                              });
+                              alert('All items have been removed from your cart!');
+                          }
+                      });
+              });
+          }
 
 
-
-
-
-
-
-
-        const deleteAllFromCartButton = document.getElementById('deleteAllFromCart');
-
-        if (deleteAllFromCartButton) {
-            deleteAllFromCartButton.addEventListener('click', function() {
-                getCart('/api/storefront/carts?include=lineItems.digitalItems.options,lineItems.physicalItems.options')
-                .then(cartData => {
-                    if (cartData.length > 0) {
-                        const cartId = cartData[0].id;
-                        const physicalItems = cartData[0].lineItems.physicalItems;
-                        const itemIds = physicalItems.map(item => item.id);
-    
-                        itemIds.forEach(itemId => {
-                            deleteCartItem('/api/storefront/carts/', cartId, itemId);
-                        });
-    
-                        alert('All items have been removed from your cart!');
-                    }
-                });
-            });
-        }
-
-
-
-        function getCart(route) {
-            return fetch(route, {
-                method: "GET",
-                credentials: "same-origin"
-            })
-            .then(response => response.json())
-            .then(result => result) // Return the cart data
-            .catch(error => console.error(error));
-        }
-        
-        function deleteCartItem(routeStart, cartId, itemId) {
-            const route = routeStart + cartId + '/items/' + itemId;
-            return fetch(route, {
-                method: "DELETE",
-                credentials: "same-origin",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            .then(response => response.json())
-            .then(result => console.log(result)) // Log the server response
-            .catch(error => console.error(error));
-        }
 
 
 
